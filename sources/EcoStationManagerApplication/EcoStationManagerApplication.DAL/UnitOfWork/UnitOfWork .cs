c2797484142751de9ffcf3635d699495f3352601
Hoang Sinh Hung <hungminhtobe@gmail.com>
2025-11-11 02:13:15 +0700

    update unitofwork

--- sources/EcoStationManagerApplication/EcoStationManagerApplication.DAL/UnitOfWork/UnitOfWork .cs
+++ sources/EcoStationManagerApplication/EcoStationManagerApplication.DAL/UnitOfWork/UnitOfWork .cs
@@ -8,7 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EcoStationManagerApplication.DAL
+namespace EcoStationManagerApplication.DAL.UnitOfWork
 {
     public class UnitOfWork : IUnitOfWork
     {
---
+++ sources/EcoStationManagerApplication/EcoStationManagerApplication.DAL/UnitOfWork/UnitOfWorkFactory.cs
@@ -0,0 +1,31 @@
+﻿using EcoStationManagerApplication.DAL.Database;
+using EcoStationManagerApplication.DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcoStationManagerApplication.DAL.UnitOfWork
+{
+    public static class UnitOfWorkFactory
+    {
+        /// <summary>
+        /// Tạo UnitOfWork mới
+        /// </summary>
+        public static IUnitOfWork Create()
+        {
+            var databaseHelper = new DatabaseHelper();
+            return new UnitOfWork(databaseHelper);
+        }
+
+        /// <summary>
+        /// Tạo UnitOfWork với connection string tùy chỉnh
+        /// </summary>
+        public static IUnitOfWork Create(string connectionString)
+        {
+            var databaseHelper = new DatabaseHelper(connectionString);
+            return new UnitOfWork(databaseHelper);
+        }
+    }
+}
